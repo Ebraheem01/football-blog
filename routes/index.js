198868a69b0f207ccad6d16f31dc2e988d9310bd
Ebraheem Seale <ebraheemseale@gmail.com>
2019-04-11 13:12:33 +0200

    working on responsiveness

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Posts = require('../db.json');
-var ReadMore = require('../db.json');
 
 
 /* GET home parge. */
@@ -10,7 +9,6 @@ router.get('/', function(req, res, next) {
 var data = {
   title: "Pro-Soccer",
   blog: Posts,
-  readMore: ReadMore,
   message: false,
 };
 
